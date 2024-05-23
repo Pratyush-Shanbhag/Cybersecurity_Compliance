8164549a4997f9d42fbe32a68186b1b0b0c8ef0c
Pratyush-Shanbhag <pratyushshanbhag10@gmail.com>
2024-05-23 13:19:11 -0700

    dynamodb post request success

--- Week_2/user-service/src/functions/create-user.js
+++ Week_2/user-service/src/functions/create-user.js
@@ -1,32 +1,42 @@
 'use strict';
 
-const AWS = require('aws-sdk')
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { PutCommand } = require('@aws-sdk/lib-dynamodb');
+
+const client = new DynamoDBClient({ region: process.env.AWS_REGION});
+
 
 module.exports.createUser = async (event, context) => {
-    const body = JSON.parse(event.body)
-    const username = body.username
-    const password = body.password
-    const newUserParams = {
-        TableName: process.env.DYNAMODB_USER_TABLE,
-        Item: {
-            pk: username,
-            password: password
-        }
+  const body = JSON.parse(event.body)
+  const username = body.username
+  const password = body.password
+  
+  const newUserParams = {
+    TableName: process.env.DYNAMODB_USER_TABLE,
+    Item: {
+      pk: username,
+      password: password
     }
-    try {
-        const dynamodb = new AWS.DynamoDB.DocumentClient()
-        const putResult = await dynamodb.put(newUserParams).promise()
+  }
 
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                message: 'Function executed successfully',
-                input: event,
-            }),
-        }
-    } catch (putError) {
-        console.log
-    }
+  try {
+    await client.send(new PutCommand(newUserParams));
 
-    
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Headers': 'Authorization'
+      },
+      body: JSON.stringify({ message: 'User created successfully' }),
+    };
+  } catch(putError) {
+    console.log('There was an error putting the new item')
+    console.log('putError', putError)
+    console.log('newUserParams', newUserParams)
+    return new Error('There was an error putting the new item')
+  }
+
+
 };
